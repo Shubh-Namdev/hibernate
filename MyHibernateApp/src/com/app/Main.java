9d46fdb86eaf3f59c249a790f9d2482e57372134
Shubham Namdev <s.namdev013@gmail.com>
2025-01-20 17:29:47 +0530

    create or save an object

--- MyHibernateApp/src/com/app/Main.java
+++ MyHibernateApp/src/com/app/Main.java
@@ -4,29 +4,22 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import com.app.dto.FourWheeler;
-import com.app.dto.TwoWheeler;
-import com.app.dto.Vehicle;
+import com.app.dto.UserDetails;
+
+
 
 public class Main {
 	public static void main(String[] args) {		
-		Vehicle vehicle = new Vehicle();
-		vehicle.setVehicleName("Vehicle");
-		
-		TwoWheeler bike = new TwoWheeler();
-		bike.setVehicleName("Yamaha FZ");
-		bike.setHandle("Bike handle");
-
-		FourWheeler car = new FourWheeler();
-		car.setVehicleName("Magnite");
-		car.setSteering("Power steering");
-		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-		session.save(vehicle);
-		session.save(bike);
-		session.save(car);
+		
+		for (int i = 1; i <= 10; i++) {
+			UserDetails user = new UserDetails();
+			user.setName("User "+i);
+			session.save(user);
+		}
+		
 		session.getTransaction().commit();
 		session.close();
 		
