2171d43ce08f172bbf0dc0303f34c82644be12fa
Shubham Namdev <s.namdev013@gmail.com>
2025-01-22 17:53:24 +0530

    first and second level cache

--- MyHibernateApp/src/com/app/Main.java
+++ MyHibernateApp/src/com/app/Main.java
@@ -1,38 +1,31 @@
 package com.app;
 
-import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.NativeQuery;
-import org.hibernate.query.Query;
 
 import com.app.dto.UserDetails;
 
 
+
 public class Main {
 	public static void main(String[] args) {
-		//String userId = "3 or 1=1";
-		String userId = "2";
-		String userName = "User 4";
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-		
-		Query<UserDetails> query1 = session.createNamedQuery("UserDetails.byId", UserDetails.class);
-		query1.setParameter("userId", Integer.parseInt(userId));
-		List<UserDetails> users = query1.list();
-		
-		Query<UserDetails> query2 = session.createNamedQuery("UserDetails.byName", UserDetails.class);
-		query2.setParameter("username", userName);
-		List<UserDetails> userss = query2.list();
-	
+		UserDetails user = session.get(UserDetails.class, 1);
+		//first level cache
+		//UserDetails user2 = session.get(UserDetails.class, 1);
 		session.getTransaction().commit();
 		session.close();
 		
-		System.out.println(users);
-		System.out.println(userss);
+		//second session
+		Session session2 = sessionFactory.openSession();
+		session2.beginTransaction();
+		UserDetails user2 = session2.get(UserDetails.class, 1);
+		session2.getTransaction().commit();
+		session2.close();
 	}
 }
